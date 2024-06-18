7b508b256a0fd8af1ccfee65cf48d53bd2a9a0e8
NhatHONGUYEN <hnnhat67@gmail.com>
2024-06-18 15:09:37 +0200

    ✨ F01 : Add our Routes for pages > Home , about , work and error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Work from "./pages/Work";
+import Contact from "./pages/Contact";
+import Error from "./pages/Error";
 
 function App() {
-  return <div className="text-white bg-red-500">salut cest moi</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Home />} />
+        <Route path="/error" element={<Error />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
