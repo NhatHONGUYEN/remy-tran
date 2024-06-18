d49b14038c142c0453622c79ae9aa2162511dac5
NhatHONGUYEN <hnnhat67@gmail.com>
2024-06-18 15:20:11 +0200

    🚧  F01 : install the navbar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,24 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-between text-2xl p-8">
+      <div>
+        <img
+          className="object-contain h-12 w-12"
+          src="../../public/Logo+Remy_Def_Noir+PNG.png"
+          alt="logo"
+        />
+      </div>
+      <div>
+        <Link className="mr-4" to="/about">
+          About
+        </Link>
+        <Link className="mr-4" to="/work">
+          Work
+        </Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </div>
+  );
 }
