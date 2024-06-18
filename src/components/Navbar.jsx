117f83667b2a8de8433a81dc4f00bdf475cba2c7
NhatHONGUYEN <hnnhat67@gmail.com>
2024-06-18 18:05:07 +0200

    ✨ F01 : Addhover on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,40 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export default function Navbar() {
   return (
     <div className="absolute w-full h-12 z-50 top-0 left-0 text-4xl ">
       <div className="flex justify-between items-center p-12 border-b border-black">
-        <div>
+        <Link to="/">
           <img
-            className="object-contain h-12 w-12"
+            className="h-20 w-20 border-2 border-black rounded-full"
             src="../../public/Logo+Remy_Def_Noir+PNG.png"
             alt="logo"
           />
-        </div>
-        <div>
-          <Link className="mr-10" to="/about">
-            About
-          </Link>
-          <Link className="mr-10" to="/work">
-            Work
-          </Link>
-          <Link to="/contact">Contact</Link>
+        </Link>
+        <div className="flex">
+          <motion.div
+            whileHover={{ scale: 1.25 }}
+            transition={{ duration: 0.3 }}
+          >
+            <Link className="mr-10" to="/about">
+              About
+            </Link>
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.25 }}
+            transition={{ duration: 0.3 }}
+          >
+            <Link className="mr-10" to="/work">
+              Work
+            </Link>
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.25 }}
+            transition={{ duration: 0.3 }}
+          >
+            <Link to="/contact">Contact</Link>
+          </motion.div>
         </div>
       </div>
     </div>
